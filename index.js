46e2c819f6dfd4d13b043965e53d5c9e0c38f02d
Miriama Svítková <miri52@users.noreply.github.com>
2021-04-12 18:01:30 +0100

    Add animation and automate card transitions
    - fade visibility when tranisitoning to the next cards
    - transition to the next cards automatically after 4 seconds
    - auto transition stops after user interacts with the carousel for the
      first time
    - fix the bug of background-blend mode in Safari by using
      linear-gradient instead

--- index.js
+++ index.js
@@ -1,43 +1,63 @@
 const btnPrev = document.querySelector(".btn-prev");
 const btnNext = document.querySelector(".btn-next");
+const indicators = document.getElementsByClassName("indicator")
 const cards = document.getElementsByClassName("cards");
 const totalCards = cards.length;
 let position = 0;
 
-function hideCards(){
+function hideActive(){
  for (card of cards){
      card.classList.remove("cards-visible");
      card.classList.add("cards-hidden")
  }
+
+ for (indicator of indicators){
+     indicator.classList.remove("indicator-active");
+ }
+}
+
+function addNextVisibile(){
+    if (position === totalCards - 1){
+        position = 0
+    } else {
+        position++
+    }
+    cards[position].classList.add("cards-visible")
+    indicators[position].classList.add("indicator-active")
+}
+
+function addPrevVisible(){
+ if (position === 0){
+        position = totalCards - 1
+    } else {
+        position--
+    }
+    cards[position].classList.add("cards-visible")
+    indicators[position].classList.add("indicator-active")
 }
 
 function showPreviousCards(){
-    hideCards();
-
-    if (position === 0){
-        position = totalCards - 1
-        console.log(position)
-    } else {
-        position--
-        console.log(position)
-    }
-    cards[position].classList.add("cards-visible")
+    stopAutoShow();
+    hideActive();
+    addPrevVisible();
 }
 
 function showNextCards(){
-
-    hideCards();
-
-    if (position === totalCards - 1){
-        position = 0
-        console.log(position)
-    } else {
-        position++
-        console.log(position)
-    }
-    cards[position].classList.add("cards-visible")
+    stopAutoShow();
+    hideActive();
+    addNextVisibile();
 }
 
+function autoShowNextCards(){
+    hideActive();
+    addNextVisibile();
+}
 
 btnPrev.addEventListener("click", showPreviousCards)
 btnNext.addEventListener("click", showNextCards)
+
+
+const intervalID = setInterval(autoShowNextCards, 4000)
+function stopAutoShow(){
+    clearInterval(intervalID)
+}
